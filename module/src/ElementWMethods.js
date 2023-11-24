d23c9c62ea6ad32ae4f93a3ff11ab38bae6c4bf1
SpcFORK <126259962+SpcFORK@users.noreply.github.com>
2023-11-24 11:25:29 +0000

    EJSify changes

--- module/src/ElementWMethods.js
+++ module/src/ElementWMethods.js
@@ -1,290 +1,55 @@
 let Methods = (cw, class_) => new Object({
 
-  SushaWrapper: ElementWrapper,
+  get __handler__() { return window[GR_SYM] },
 
-  element: this?.element || this,
+  syncHash() {
+    // @ CREDIT (FORK): juniorrantila
+    state().id = 0;
 
-  __LOREM__() { return ElementWrapper.LOREM },
-  get LOREM() { return this.__LOREM__() },
+    url = locations.url();
+    hashLocation = locations.hashLocation(url);
 
-  att$(name, value) {
-    if (Array.isArray(value)) {
-      value = value.join(' ');
+    const route404 = '/404';
+
+    if (!(hashLocation in routes)) {
+      console.assert(route404 in routes, `Route "${route404}" not found among the routes.`);
+      hashLocation = route404;
     }
 
-    cw.element.setAttribute(name, value);
-    return cw;
+    result.replaceChildren(routes[hashLocation](result));
+    currentLocation.value = hashLocation;
+
+    // @ Window Handler
+    window[GC_SYM]?.syncHash?.(hashLocation);
+
+    return result;
   },
 
-  onclick$(callback) {
-    cw.element.onclick = callback;
-    return cw;
+  destroyGrechaSync() {
+    window.removeEventListener("hashchange", methods.syncHash);
   },
 
-  get$() {
-    return cw.element;
+  deleteGrecha() {
+    delete window[g_];
+    for (const method in methods) delete window[method];
+    delete window[GR_SYM];
   },
 
-  wrapper$() {
-    return cw;
-  },
+  // @ CREDIT (FORK): juniorrantila
+  useState(initialValue) {
+    const id = state().id++;
 
-  // @ Tag Wrapper
-  wrap$(t) {
-    // Contains <b data-grecha>?
-    // Get Only child
+    state()[id] = state()[id] ?? initialValue;
 
-    let w = cw.element.querySelector('[data-grecha="wrap$' + t + '"]');
-
-    // has attr ?
-    if (!w) {
-      let b = new ElementWrapper(t);
-
-      if (t && t instanceof HTMLElement) {
-        b._changeElement(t);
+    return [
+      () => state()[id],
+      (v) => {
+        state()[id] = v;
+        result.refresh();
       }
-
-      b.att$('data-grecha', 'wrap$' + t);
-
-      b.element.innerHTML = cw.element.innerHTML
-      cw.element.innerHTML = '';
-      cw.element.appendChild(b.element);
-
-      return cw.element;
-    } else {
-
-      // Get wrapped element
-
-      console.log(w);
-      cw.element.innerHTML = w.innerHTML;
-      w.innerHTML = '';
-
-      // Remove w node from Document.
-      w?.remove?.()
-
-      return cw.element;
-
-    }
+    ];
   },
-
-  bold$() {
-    return cw.wrap$('b')
-
-  },
-
-  italic$() {
-    return cw.wrap$('i')
-
-  },
-
-  underline$() {
-    return cw.wrap$('u')
-
-  },
-
-  strike$() {
-    return cw.wrap$('s')
-
-  },
-
-  // Into Link
-  link$(href, text) {
-    let wrapped = this.wrap$('a')
-
-    if (wrapped && (href || text)) {
-      wrapped.firstChild.att$('href', href)
-      wrapped.firstChild.att$('target', '_blank')
-      wrapped.firstChild.att$('rel', 'noopener noreferrer')
-      wrapped.firstChild.att$('title', text)
-
-      return wrapped
-    }
-  },
-
-  // Set XY
-  absolutePos$(x, y) {
-    cw.element.style.position = 'absolute';
-    cw.element.style.display = 'flex';
-    cw.element.style.flexWrap = 'wrap';
-
-    cw.element.style.left = x;
-    cw.element.style.top = y;
-  },
-
-  // Reset Position
-  resetPos$() {
-    cw.element.style.position = 'static';
-    cw.element.style.display = 'block';
-    cw.element.style.flexWrap = 'nowrap';
-
-    cw.element.style.left = '';
-    cw.element.style.top = '';
-  },
-
-  // Register a new attribute for a custom data type
-  data$(key, value) {
-    cw.att$(`data-${key}`, value);
-    return cw;
-  },
-
-  // Append a child element
-  append$(child) {
-    if (child instanceof ElementWrapper) {
-      cw.element.appendChild(child.element);
-    } else if (child instanceof HTMLElement) {
-      cw.element.appendChild(child);
-    } else if (typeof child === 'string') {
-      cw.element.appendChild(document.createTextNode(child));
-    }
-    return cw;
-  },
-
-  // Set ID attribute
-  id$(id) {
-    cw.att$('id', id);
-    return cw;
-  },
-
-  // Set classes
-  class$(...classes) {
-    cw.element.classList.add(...classes);
-    return cw;
-  },
-
-  // Set styles directly on an element
-  style$(styleObject) {
-    for (const property in styleObject) {
-      cw.element.style[property] = styleObject[property];
-    }
-    return cw;
-  },
-
-  // Toggle class on an element
-  toggleClass$(className) {
-    cw.element.classList.toggle(className);
-    return cw;
-  },
-
-  // Set inner HTML
-  html$(htmlContent) {
-    cw.element.innerHTML = htmlContent;
-    return cw;
-  },
-
-  // Set inner Text
-  text$(textContent) {
-    cw.element.textContent = textContent;
-    return cw;
-  },
-
-  // Additional methods to be included in the ElementWrapper class
-
-  // Set a placeholder attribute for an input element
-  placeholder$(placeholderValue) {
-    cw.att$('placeholder', placeholderValue);
-    return cw;
-  },
-
-  // Set the type attribute for an input or button element
-  type$(typeValue) {
-    cw.att$('type', typeValue);
-    return cw;
-  },
-
-  // Add an event listener
-  on$(event, handler) {
-    cw.element.addEventListener(event, handler);
-    return cw;
-  },
-
-  // Remove an event listener
-  off$(event, handler) {
-    cw.element.removeEventListener(event, handler);
-    return cw;
-  },
-
-  // Perform an action upon pressing the Enter key
-  onEnter$(callback) {
-    cw.on$('keydown', (e) => {
-      if (e.key === 'Enter') callback(e);
-    });
-    return cw;
-  },
-
-  // Focus the element
-  focus$() {
-    cw.element.focus();
-    return cw;
-  },
-
-  // Blur the element
-  blur$() {
-    cw.element.blur();
-    return cw;
-  },
-
-  // Add an attribute for accessibility purposes
-  aria$(key, value) {
-    cw.att$(`aria-${key}`, value);
-    return cw;
-  },
-
-  // Append multiple children at once
-  appendChildren$(...children) {
-    children.forEach((child) => {
-      cw.append$(child);
-    });
-    return cw;
-  },
-
-  // Set the disabled attribute on an element
-  disabled$(isDisabled) {
-    if (isDisabled) {
-      cw.att$('disabled', 'true');
-    } else {
-      cw.element.removeAttribute('disabled');
-    }
-    return cw;
-  },
-
-  // Create and append a new child element
-  createElement$(tag, ...children) {
-    cw.append$(new ElementWrapper(tag, ...children));
-    return cw;
-  },
-
-  // Fetch and display content - Example: Asynchronous operation to load data
-  fetchContent$(url, processContentCallback) {
-    fetch(url)
-      .then(response => response.json())
-      .then(data => processContentCallback(cw, data))
-      .catch(error => console.error('Error:', error));
-    return cw;
-  },
-
-  stylesheet$() {
-    // Stylesheets are cached in the wrapper
-    if (cw?._stylesheet) {
-      return cw._stylesheet;
-    }
-
-    // Create a new stylesheet
-    const stylesheet = new ElementWrapper(document.createElement('style'));
-
-    // Append the stylesheet to the wrapper
-    this.append$(stylesheet);
-  },
-
-  bounds$() {
-    return cw.element.getBoundingClientRect();
-  },
-
-  str$() {
-    return (
-      cw.element?.outerHTML
-      || cw.element?.innerHTML
-    );
-  },
+  // ---
 
 })
 
