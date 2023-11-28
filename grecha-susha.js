fd76f9b7537764a67ee75060daeddf545de1c243
SpcFORK <126259962+SpcFORK@users.noreply.github.com>
2023-11-28 03:11:54 +0000

    Fix URL state err;

--- grecha-susha.js
+++ grecha-susha.js
@@ -455,6 +455,210 @@ class Grecha {
     }
     // ---
 
+
+
+    async function hydrate() {
+
+      // Get every href attribute
+      let hrefs = document.querySelectorAll('[href]');
+      let urls = document.querySelectorAll('[url]');
+      let srcs = document.querySelectorAll('[src]');
+
+      // ['/', './', '../'] => 'https://<origin>/'
+      let origin = window.location.origin;
+      let path = window.location.pathname;
+      let pathArray = path.split('/');
+      let pathArrayLength = pathArray.length;
+
+      let urlModel = {
+        href: hrefs,
+        url: urls,
+        src: srcs,
+      }
+
+      let entries = Object.entries(urlModel);
+
+      for (let i = 0; i < entries.length; i++) {
+        let entry = entries[i];
+        let entryName = entry[0];
+        let entryArray = entry[1];
+
+        // Loop through every entry
+        a: for (let j = 0; j < entryArray.length; j++) {
+          let element = entryArray[j];
+          let href = element.getAttribute('href');
+          // If 'a' tag, attach 'a' click event hydrator;
+          if (element.tagName == 'A') {
+
+            let urlArray = href.split('/');
+            let url_ = href;
+
+            // Has Protocol prefix?
+            let hasProtocol = (
+              (urlArray[0] + '').includes(':')
+              // Since // is 2 /, we check if we have 1 empty item
+              && (urlArray[1] + '') == ''
+            );
+
+            // console.log(url_)
+
+            if (!hasProtocol) {
+              url_ = origin + '/' + href;
+            }
+            
+            let resURL = new URL(url_);
+            
+            // If starts with '#/', return because is Router route
+            if (resURL.hash.startsWith('#/') || (href.startsWith('#'))) {
+              continue a;
+            }
+
+            else {
+              element.addEventListener('click', async (e) => {
+                e.preventDefault();
+                e.stopImmediatePropagation();
+                let data = Grecha.loaded[href];
+                // Write HTML to DOM
+                let html = data;
+
+                if (!html) {
+                  // Load Loading HTML template.
+                  HTMLRoot.innerHTML = SushaTemplates.loadinglinks.innerHTML;
+                  html = await fetch(href).then(res => res.text());
+                }
+
+                // Replace 'href="./..."' && 'href="../..."' with 'href="<url>/..."'
+                html = html.replace(/(href|src|url)="(?:\.\/)?(.*?)"/g, (match, p1, p2) => {
+                  if (p2.startsWith('../')) {
+                    // Replace all occurrences of ../ with the next parent path
+                    let parentCount = (p2.match(/\.\.\//g) || []).length;
+                    let urlArrayCopy = urlArray.slice(0, urlArray.length - parentCount);
+                    p2 = p2.replace(/^(\.\.\/)/, origin + '/');
+                    p2 = p2.replace(/\.\.\//g, '');
+                    let innards = urlArrayCopy.join('/');
+                    return `${p1}="${innards ? innards + '/' : ''}${p2}"`;
+                  }
+                  return `${p1}="${origin}/${href}/${p2}"`;
+                });
+
+                if (html) document.write(html);
+
+                // Scroll to top
+                window.scrollTo(0, 0);
+                // Set URL to HREF without reloading page
+
+                // Get every Script
+                let scripts = document.querySelectorAll('script');
+                for (let k = 0; k < scripts.length; k++) {
+                  let script = scripts[k];
+                  let src = script.getAttribute('src');
+
+                  if (src) {
+                    let res = await fetch(src);
+                    let text = await res.text();
+
+                    // Remove Script && append new Script
+                    let reScript = document.createElement('script');
+
+                    for (let l = 0; l < script.attributes.length; l++) {
+                      let attr = script.attributes[l];
+                      reScript.setAttribute(attr.name, attr.value);
+                    }
+
+                    script.remove();
+                    reScript.innerHTML = text;
+                    document.body.appendChild(reScript);
+                  }
+
+                  else if (script.text) {
+                    // Append new Script
+                    let reScript = document.createElement('script');
+
+                    for (let l = 0; l < script.attributes.length; l++) {
+                      let attr = script.attributes[l];
+                      reScript.setAttribute(attr.name, attr.value);
+                    }
+
+                    document.body.appendChild(reScript);
+                  }
+                }
+
+                // Send fake DOM load event
+                let fakeEvent = new Event('DOMContentLoaded');
+                window.dispatchEvent(fakeEvent);
+
+              });
+            }
+          }
+
+          Grecha.preloaded.push({
+            tag: element,
+            attribute: entryName,
+            value: element.getAttribute(entryName)
+          });
+        }
+      }
+
+      if (Grecha.preloaded.length < 1) {
+        console.info('[GS Optimizer] No elements found to preload.');
+        return;
+      }
+
+      // Loop through every preloaded element
+      await new Promise(async (resolve, reject) => {
+        for (let i = 0; i < Grecha.preloaded.length; i++) {
+          let element = Grecha.preloaded[i];
+
+          // Construct URL
+          let url = element.value;
+          let urlArray = url.split('/');
+
+          // Has Protocol prefix?
+          let hasProtocol = (
+            (urlArray[0] + '').includes(':')
+            // Since // is 2 /, we check if we have 1 empty item
+            && (urlArray[1] + '') == ''
+          );
+
+          if (!hasProtocol) {
+            url = origin + '/' + url;
+          }
+
+          let url_ = new URL(url);
+          let groupsOfPathPeriods = url_.pathname.split('.');
+
+          if (groupsOfPathPeriods.length > 1) {
+            let fileExtention = groupsOfPathPeriods[groupsOfPathPeriods.length - 1];
+
+            switch (fileExtention) {
+              case 'html':
+                break;
+              default: return
+            }
+          }
+
+          // If starts with '#/', return because is Router route
+          if (url_.hash.startsWith('#/')) {
+            return;
+          }
+
+          else if (url_.pathname.startsWith('/data:')) {
+            return
+          }
+
+          // Fetch Element
+          let fetchedElement = await fetch(url);
+          let fetchedElementText = await fetchedElement.text();
+
+          Grecha.loaded.push({
+            tag: element,
+            attribute: element.attribute,
+            value: fetchedElementText
+          });
+        }
+      })
+    }
+    
     // @ Tagware, @wm
     const windowMethods = {
 
@@ -764,6 +968,7 @@ class Grecha {
 
             // @ Window Handler
             window[GC_SYM]?.syncHash?.(hashLocation);
+            hydrate()
 
             return result;
           },
@@ -1148,7 +1353,6 @@ class Grecha {
             // Rule: line 2: 3 symbols + space + same as match 1
 
             let src = prefs?.code || await getFile();
-            // console.log(src);
             prefs.src = src;
 
             if (!prefs) {
@@ -1159,8 +1363,6 @@ class Grecha {
               /^\/[*/] *([^\s]*?[\W]){3} *authorheader *\1{3} *(?:\*\/)*([^]+)\/[*/] *\1{3} *\1{3} *(?:\*\/)*/gim;
 
             let matchees = regex.exec(prefs.src)?.[2];
-
-            // console.log(matchees);
 
             if (!matchees) {
               return fulldata;
@@ -1177,13 +1379,10 @@ class Grecha {
               let regex = /^\/\/ *([^\n]*?) *(?:\n|$)/gim;
 
               let matchees = comments.match(regex);
-              // console.log(matchees, 'cmprt');
               let formatted = [];
 
               matchees.forEach((match) => {
-                // console.log(match);
                 let formattedMatch = match.replace(/\n/g, "");
-                // console.log(formattedMatch);
 
                 if (formattedMatch.startsWith("/")) {
                   if (formattedMatch.startsWith("//")) {
@@ -2555,212 +2754,7 @@ class Grecha {
 
     windowOptimizer();
 
-    document.addEventListener('DOMContentLoaded', async () => {
-
-      // Get every href attribute
-      let hrefs = document.querySelectorAll('[href]');
-      let urls = document.querySelectorAll('[url]');
-      let srcs = document.querySelectorAll('[src]');
-
-      // ['/', './', '../'] => 'https://<origin>/'
-      let origin = window.location.origin;
-      let path = window.location.pathname;
-      let pathArray = path.split('/');
-      let pathArrayLength = pathArray.length;
-
-      let urlModel = {
-        href: hrefs,
-        url: urls,
-        src: srcs,
-      }
-
-      let entries = Object.entries(urlModel);
-
-      for (let i = 0; i < entries.length; i++) {
-        let entry = entries[i];
-        let entryName = entry[0];
-        let entryArray = entry[1];
-
-        // Loop through every entry
-        a: for (let j = 0; j < entryArray.length; j++) {
-          let element = entryArray[j];
-          let href = element.getAttribute('href');
-          // If 'a' tag, attach 'a' click event hydrator;
-          if (element.tagName == 'A') {
-
-            let urlArray = href.split('/');
-            let url_ = href;
-
-            // Has Protocol prefix?
-            let hasProtocol = (
-              (urlArray[0] + '').includes(':')
-              // Since // is 2 /, we check if we have 1 empty item
-              && (urlArray[1] + '') == ''
-            );
-
-            // console.log(url_)
-
-            if (!hasProtocol) {
-              url_ = origin + '/' + href;
-            }
-
-            url_ = new URL(url_);
-
-            // If starts with '#/', return because is Router route
-            if (url_.hash.startsWith('#/')) {
-              continue a;
-            }
-
-            else {
-              element.addEventListener('click', async (e) => {
-                e.preventDefault();
-                e.stopImmediatePropagation();
-                let data = Grecha.loaded[href];
-                // Write HTML to DOM
-                let html = data;
-
-                if (!html) {
-                  // Load Loading HTML template.
-                  HTMLRoot.innerHTML = SushaTemplates.loadinglinks.innerHTML;
-                  html = await fetch(href).then(res => res.text());
-                }
-
-                // Replace 'href="./..."' && 'href="../..."' with 'href="<url>/..."'
-                html = html.replace(/(href|src|url)="(?:\.\/)?(.*?)"/g, (match, p1, p2) => {
-                  if (p2.startsWith('../')) {
-                    // Replace all occurrences of ../ with the next parent path
-                    let parentCount = (p2.match(/\.\.\//g) || []).length;
-                    let urlArrayCopy = urlArray.slice(0, urlArray.length - parentCount);
-                    p2 = p2.replace(/^(\.\.\/)/, origin + '/');
-                    p2 = p2.replace(/\.\.\//g, '');
-                    let innards = urlArrayCopy.join('/');
-                    return `${p1}="${innards ? innards + '/' : ''}${p2}"`;
-                  }
-                  return `${p1}="${origin}/${href}/${p2}"`;
-                });
-
-                // html = html.replace(/(href|src|url)="\.\.\/(.*?)"/g, (match, p1, p2) => {
-                //   console.log(p2)
-                //   return `${p1}="${href}/${urlArray.slice(0, urlArray.length - 1).join('/')}/${p2}"`;
-                // });
-
-                if (html) document.write(html);
-
-                // Scroll to top
-                window.scrollTo(0, 0);
-                // Set URL to HREF without reloading page
-
-                // Get every Script
-                let scripts = document.querySelectorAll('script');
-                for (let k = 0; k < scripts.length; k++) {
-                  let script = scripts[k];
-                  let src = script.getAttribute('src');
-
-                  if (src) {
-                    let res = await fetch(src);
-                    let text = await res.text();
-
-                    // Remove Script && append new Script
-                    let reScript = document.createElement('script');
-
-                    for (let l = 0; l < script.attributes.length; l++) {
-                      let attr = script.attributes[l];
-                      reScript.setAttribute(attr.name, attr.value);
-                    }
-
-                    script.remove();
-                    reScript.innerHTML = text;
-                    document.body.appendChild(reScript);
-                  }
-
-                  else if (script.text) {
-                    // Append new Script
-                    let reScript = document.createElement('script');
-
-                    for (let l = 0; l < script.attributes.length; l++) {
-                      let attr = script.attributes[l];
-                      reScript.setAttribute(attr.name, attr.value);
-                    }
-
-                    document.body.appendChild(reScript);
-                  }
-                }
-
-                // Send fake DOM load event
-                let fakeEvent = new Event('DOMContentLoaded');
-                window.dispatchEvent(fakeEvent);
-
-              });
-            }
-          }
-
-          Grecha.preloaded.push({
-            tag: element,
-            attribute: entryName,
-            value: element.getAttribute(entryName)
-          });
-        }
-      }
-
-      if (Grecha.preloaded.length < 1) {
-        console.info('[GS Optimizer] No elements found to preload.');
-        return;
-      }
-
-      // Loop through every preloaded element
-      await new Promise(async (resolve, reject) => {
-        for (let i = 0; i < Grecha.preloaded.length; i++) {
-          let element = Grecha.preloaded[i];
-
-          // Construct URL
-          let url = element.value;
-          let urlArray = url.split('/');
-
-          // Has Protocol prefix?
-          let hasProtocol = (
-            (urlArray[0] + '').includes(':')
-            // Since // is 2 /, we check if we have 1 empty item
-            && (urlArray[1] + '') == ''
-          );
-
-          if (!hasProtocol) {
-            url = origin + '/' + url;
-          }
-
-          let url_ = new URL(url);
-          let groupsOfPathPeriods = url_.pathname.split('.');
-
-          if (groupsOfPathPeriods.length > 1) {
-            let fileExtention = groupsOfPathPeriods[groupsOfPathPeriods.length - 1];
-
-            switch (element.attribute) {
-              case 'html':
-                break;
-              default: return
-            }
-          }
-
-          // If starts with '#/', return because is Router route
-          if (url_.hash.startsWith('#/')) {
-            return;
-          }
-
-          else if (url_.pathname.startsWith('/data:')) {
-            return
-          }
-
-          // Fetch Element
-          let fetchedElement = await fetch(url);
-          let fetchedElementText = await fetchedElement.text();
-
-          Grecha.loaded.push({
-            tag: element,
-            attribute: element.attribute,
-            value: fetchedElementText
-          });
-        }
-      })
-    })
+    document.addEventListener('DOMContentLoaded', hydrate)
 
   }
 }
